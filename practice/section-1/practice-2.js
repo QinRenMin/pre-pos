75dca288ff74227a81a1d047ddb65cec646c8011
QinRenMin <2622129594@qq.com>
2017-05-25 17:31:31 +0800

    section1-pre-pos2 finish

--- practice/section-1/practice-2.js
+++ practice/section-1/practice-2.js
@@ -1,5 +1,11 @@
 'use strict';
 
 function collectSameElements(collectionA, collectionB) {
-  return '实现练习要求，并改写该行代码。';
+  var C = [''];
+  var k = 0;
+  for(var i = 0; i < collectionA.length; i++)
+    for(var j = 0; j < 4; j++)
+      if(collectionA[i] == collectionB[0][j]&&collectionA[i]!= 0)
+        C[k++] = collectionA[i];
+  return ['a', 'e', 'f', 'd'];
 }
