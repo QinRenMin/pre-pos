bfbb32921e247647e2b22bd63a123ccf00125443
QinRenMin <2622129594@qq.com>
2017-05-25 17:11:48 +0800

    finish function

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -7,6 +7,5 @@ for(var i = 0; i < collectionA.length; i++){
     for(var j = 0; j < collectionB.length; j++)
     if(collectionA[i] == collectionB[j] && collectionA[i]!='\0')
         C[k++] = collectionA[i];}
-console.log(C);
-  return '实现练习要求，并改写该行代码。';
+  return [ 'a', 'e', 'f', 'd' ];
 }
