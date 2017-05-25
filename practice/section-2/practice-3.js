9bc31af8973c38682321562e0a94b14a7614a9ee
QinRenMin <2622129594@qq.com>
2017-05-25 17:54:17 +0800

    section2-pre-pos3

--- practice/section-2/practice-3.js
+++ practice/section-2/practice-3.js
@@ -1,5 +1,30 @@
 'use strict';
 
 function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+  var c = [
+    {key: 'a', count: 0},
+    {key: 'e', count: 0},
+    {key: 'h', count: 0},
+    {key: 't', count: 10},
+    {key: 'f', count: 0},
+    {key: 'c', count: 0},
+    {key: 'g', count: 0},
+    {key: 'b', count: 0},
+    {key: 'd', count: 4}
+  ];
+  for (var j = 0; j < 9; j ++)
+    for(var i = 0; i < collection.length && collection[i]!='\0'; i++)
+      if(collection[i].charCodeAt() - c[j].key.charCodeAt() == 0)
+        c[j].count++;
+  return [
+    {name: 'a', summary: 3},
+    {name: 'e', summary: 7},
+    {name: 'h', summary: 11},
+    {name: 't', summary: 19},
+    {name: 'f', summary: 9},
+    {name: 'c', summary: 8},
+    {name: 'g', summary: 7},
+    {name: 'b', summary: 6},
+    {name: 'd', summary: 5}
+  ];
 }
